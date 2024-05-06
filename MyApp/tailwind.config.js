6344063a65b1ae5f5500774a5e87eb6df45dd061
Vijay Kumar Kotagiri <vijay.kotagiri@imotionsglobal.com>
2024-05-06 19:10:36 +0530

    Fixed issue with duplicate module.exports in tailwind.config.js

--- MyApp/tailwind.config.js
+++ MyApp/tailwind.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-  content: ["./**/*.{html,js,mjs,md,cshtml,razor,cs}","./Pages/**/*.{cshtml,razor}"],
-  darkMode: 'class',
-  plugins: [],
-}
-
-module.exports = {
+    content: ["./**/*.{html,js,mjs,md,cshtml,razor,cs}","./Pages/**/*.{cshtml,razor}"],
+    darkMode: 'class',
     theme: {
         extend: {
-            
+    
         },
         fontFamily: {
             sans: ['"Open Sans"', 'sans-serif'],
         },
     },
+    plugins: [],
 }
