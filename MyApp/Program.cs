3401bc2a4aea1407cd3e0df842e577410fdbda6e
Harindra <Harindra1278@gmail.com>
2024-08-01 18:13:27 +0530

    Recaptcha in contact us form....!!!

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using AspNetCore.ReCaptcha;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var mvcBuilder = builder.Services.AddRazorPages();
@@ -6,7 +8,7 @@ if (builder.Environment.IsDevelopment())
 {
     mvcBuilder.AddRazorRuntimeCompilation();
 }
-
+builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
